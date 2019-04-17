0c09d870a0001153296d2fa78d5939bf4397254b
Spencer <swhite215@live.com>
2019-04-17 10:28:23 -0400

    creating components to use

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,9 +5,25 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { FormsModule } from "@angular/forms";
+import { ShoppingListComponent } from "./components/shopping-list/shopping-list.component";
+import { ShoppingListEditComponent } from "./components/shopping-list-edit/shopping-list-edit.component";
+import { RecipeListComponent } from "./components/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from "./components/recipe-item/recipe-item.component";
+import { RecipeDetailComponent } from "./components/recipe-detail/recipe-detail.component";
+import { HeaderComponent } from "./layout/header.component";
+import { RecipesComponent } from './components/recipes/recipes.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
