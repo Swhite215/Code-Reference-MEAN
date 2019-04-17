3509e0e4c2d3ef68263c0745e68a8da714065d7b
Spencer <swhite215@live.com>
2019-04-17 14:23:28 -0400

    used references and emitted event upward

--- angular-app/src/app/components/shopping-list-edit/shopping-list-edit.component.ts
+++ angular-app/src/app/components/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { Ingredient } from "../../models/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-list-edit',
-  templateUrl: './shopping-list-edit.component.html',
-  styleUrls: ['./shopping-list-edit.component.css']
+  selector: "app-shopping-list-edit",
+  templateUrl: "./shopping-list-edit.component.html",
+  styleUrls: ["./shopping-list-edit.component.css"]
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild("nameInput") nameInputReference: ElementRef;
+  @ViewChild("amountInput") amountInputReference: ElementRef;
 
-  constructor() { }
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onAddItem() {
+    let name = this.nameInputReference.nativeElement.value;
+    let amount = this.amountInputReference.nativeElement.value;
+    const ingredient = new Ingredient(name, amount);
+
+    this.ingredientAdded.emit(ingredient);
   }
-
 }
