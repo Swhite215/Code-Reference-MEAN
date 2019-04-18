e8763460c1c1d1e52b43fdc93eb4f7157ee7f9a5
Spencer <swhite215@live.com>
2019-04-18 09:12:34 -0400

    used service to get list of recipes for component

--- angular-app/src/app/components/recipe-list/recipe-list.component.ts
+++ angular-app/src/app/components/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { Recipe } from "../../models/recipe.model";
+import { RecipeService } from "src/app/services/recipe.service";
 
 @Component({
   selector: "app-recipe-list",
@@ -8,18 +9,13 @@ import { Recipe } from "../../models/recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeToRender = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe(
-      "Chicken Kiev",
-      "A delicious 30-minute chicken dinner.",
-      "https://upload.wikimedia.org/wikipedia/commons/0/08/Chicken_Kiev_-_Ukrainian_East_Village_restaurant.jpg"
-    )
-  ];
+  constructor(private recipeService: RecipeService) {}
 
-  constructor() {}
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   recipeSelected(recipe: Recipe) {
     this.recipeToRender.emit(recipe);
