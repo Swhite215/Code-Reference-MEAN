4bd561fa63882122685a1d28b7ef75a54933ddd4
Spencer <swhite215@live.com>
2019-04-18 09:48:41 -0400

    added placeholder data for a recipe

--- angular-app/src/app/services/recipe.service.ts
+++ angular-app/src/app/services/recipe.service.ts
@@ -1,12 +1,18 @@
 import { Recipe } from "../models/recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../models/ingredient.model";
 
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
       "Chicken Kiev",
       "A delicious 30-minute chicken dinner.",
-      "https://upload.wikimedia.org/wikipedia/commons/0/08/Chicken_Kiev_-_Ukrainian_East_Village_restaurant.jpg"
+      "https://upload.wikimedia.org/wikipedia/commons/0/08/Chicken_Kiev_-_Ukrainian_East_Village_restaurant.jpg",
+      [
+        new Ingredient("chicken", 4),
+        new Ingredient("garlic", 2),
+        new Ingredient("parsley", 1)
+      ]
     )
   ];
 
