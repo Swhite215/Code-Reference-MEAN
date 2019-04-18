a296e0db251fd1e675a2326f4a228a2bbc7aaf9d
Spencer <swhite215@live.com>
2019-04-18 09:36:01 -0400

    shopping service with properties and methods

---
+++ angular-app/src/app/services/shopping.service.ts
@@ -0,0 +1,20 @@
+import { Ingredient } from "../models/ingredient.model";
+import { EventEmitter } from "@angular/core";
+
+export class ShoppingService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  ingredients: Ingredient[] = [
+    new Ingredient("apple", 10),
+    new Ingredient("cake", 1)
+  ];
+
+  getIngredients() {
+    return [...this.ingredients];
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients = [...this.ingredients, ingredient];
+    this.ingredientsChanged.emit([...this.ingredients]);
+  }
+}
