a5319a66fac4c8fdda604a35d6675d847cdbbbaa
Spencer <swhite215@live.com>
2019-04-19 12:36:06 -0400

    subscription to click event and form population

--- angular-app/src/app/components/shopping-list-edit/shopping-list-edit.component.ts
+++ angular-app/src/app/components/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy, ViewChild } from "@angular/core";
 import { Ingredient } from "../../models/ingredient.model";
 import { ShoppingService } from "../../services/shopping.service";
 import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-shopping-list-edit",
   templateUrl: "./shopping-list-edit.component.html",
   styleUrls: ["./shopping-list-edit.component.css"]
 })
-export class ShoppingListEditComponent implements OnInit {
+export class ShoppingListEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode: boolean = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
+  @ViewChild("f") shoppingListForm: NgForm;
+
   constructor(private shoppingService: ShoppingService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.shoppingService.startedEditing.subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+        this.editedItem = this.shoppingService.getIngredientByIndex(index);
+
+        this.shoppingListForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
+      }
+    );
+  }
 
   onAddItem(form: NgForm) {
     const value = form.value;
@@ -20,5 +40,9 @@ export class ShoppingListEditComponent implements OnInit {
     const ingredient = new Ingredient(name, amount);
 
     this.shoppingService.addIngredient(ingredient);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
