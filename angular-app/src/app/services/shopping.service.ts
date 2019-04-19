c925301f27981517e3cfe4c3573dd03d270aaff9
Spencer <swhite215@live.com>
2019-04-19 12:35:40 -0400

    method to get ingredient by index for editing

--- angular-app/src/app/services/shopping.service.ts
+++ angular-app/src/app/services/shopping.service.ts
@@ -1,8 +1,10 @@
 import { Ingredient } from "../models/ingredient.model";
 import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 
 export class ShoppingService {
   ingredientsChanged = new EventEmitter<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   ingredients: Ingredient[] = [
     new Ingredient("apple", 10),
@@ -21,5 +23,9 @@ export class ShoppingService {
   addToShoppingList(ingredients: Ingredient[]) {
     this.ingredients = [...this.ingredients, ...ingredients];
     this.ingredientsChanged.emit([...this.ingredients]);
+  }
+
+  getIngredientByIndex(index: number) {
+    return [...this.ingredients][index];
   }
 }
