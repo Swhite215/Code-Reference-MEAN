ab45feb62a6b442f52185d308edc7504619d4176
Spencer <swhite215@live.com>
2019-04-20 11:21:47 -0400

    placeholders for type casting of returned values

--- angular-app/src/app/services/data-storage.service.ts
+++ angular-app/src/app/services/data-storage.service.ts
@@ -8,24 +8,24 @@ export class DataStorageService {
   updateData(data: any) {
     let headers = new HttpHeaders({ "content-type": "application/json" });
     let url = `http://domain.com/path?query=true`;
-    return this.http.put(url, data, { headers: headers });
+    return this.http.put<type>(url, data, { headers: headers });
   }
 
   fetchData() {
     let headers = new HttpHeaders({ "content-type": "application/json" });
     let url = `http://domain.com/path?query=true`;
-    return this.http.get(url, { headers: headers });
+    return this.http.get<type>(url, { headers: headers });
   }
 
   createData(data: any) {
     let headers = new HttpHeaders({ "content-type": "application/json" });
     let url = `http://domain.com/path?query=true`;
-    return this.http.post(url, data, { headers: headers });
+    return this.http.post<type>(url, data, { headers: headers });
   }
 
   deleteData(id: number) {
     let headers = new HttpHeaders({ "content-type": "application/json" });
     let url = `http://domain.com/${id}`;
-    return this.http.delete(url, { headers: headers });
+    return this.http.delete<type>(url, { headers: headers });
   }
 }
