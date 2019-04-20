82de3a9bd827d65dbd0bac02280e21003754fa99
Spencer <swhite215@live.com>
2019-04-20 13:17:03 -0400

    removed unneccessary imports

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { RecipesComponent } from "./components/recipes/recipes.component";
 import { ShoppingListComponent } from "./components/shopping-list/shopping-list.component";
-import { RecipeDetailComponent } from "./components/recipe-detail/recipe-detail.component";
-import { RecipeStartComponent } from "./components/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./components/recipe-edit/recipe-edit.component";
-
 const routes: Routes = [
   { path: "", redirectTo: "recipes", pathMatch: "full" },
   { path: "shopping", component: ShoppingListComponent }
