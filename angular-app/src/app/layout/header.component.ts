53940a082389c78212b7e7d4364dcfbf6ba4cf06
Spencer <swhite215@live.com>
2019-04-20 10:48:07 -0400

    CRUD example methods that subscribe to http methds

--- angular-app/src/app/layout/header.component.ts
+++ angular-app/src/app/layout/header.component.ts
@@ -1,8 +1,49 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../services/data-storage.service";
+import { Recipe } from "../models/recipe.model";
+import { Z_DATA_ERROR } from "zlib";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"]
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onUpdateData(data: any) {
+    this.dataStorageService
+      .updateData(data)
+      .subscribe(
+        (response: Response) => console.log(response),
+        (error: Response) => console.log(error)
+      );
+  }
+
+  onCreateData(data: any) {
+    this.dataStorageService
+      .createData(data)
+      .subscribe(
+        (response: Response) => console.log(response),
+        (error: Response) => console.log(error)
+      );
+  }
+
+  onFetchData() {
+    this.dataStorageService
+      .fetchData()
+      .subscribe(
+        (response: Response) => console.log(response),
+        (error: Response) => console.log(error)
+      );
+  }
+
+  onDeleteData(id: number) {
+    this.dataStorageService
+      .deleteData(id)
+      .subscribe(
+        (response: Response) => console.log(response),
+        (error: Response) => console.log(error)
+      );
+  }
+}
