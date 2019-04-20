f7a226a582dc183367d6b244699e86e9baf89887
Spencer <swhite215@live.com>
2019-04-20 13:03:00 -0400

    cleaned up and passed two new modules

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,50 +3,40 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { RecipesModule } from "./modules/recipes.module";
 
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { ShoppingListComponent } from "./components/shopping-list/shopping-list.component";
 import { ShoppingListEditComponent } from "./components/shopping-list-edit/shopping-list-edit.component";
-import { RecipeListComponent } from "./components/recipe-list/recipe-list.component";
-import { RecipeItemComponent } from "./components/recipe-item/recipe-item.component";
-import { RecipeDetailComponent } from "./components/recipe-detail/recipe-detail.component";
 import { HeaderComponent } from "./layout/header.component";
-import { RecipesComponent } from "./components/recipes/recipes.component";
 import { BasicHighlightDirective } from "./directives/basic-highlight.directive";
 import { BetterHighlightDirective } from "./directives/better-highlight.directive";
 import { UnlessDirective } from "./directives/unless.directive";
 import { DropDownDirective } from "./directives/dropdown.directive";
 import { ShoppingService } from "./services/shopping.service";
-import { RecipeStartComponent } from "./components/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./components/recipe-edit/recipe-edit.component";
 import { RecipeService } from "./services/recipe.service";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { DataStorageService } from "./services/data-storage.service";
 import { AuthInterceptor } from "./interceptors/auth.interceptor";
+import { SharedDirectiveModule } from "./modules/shared-directive.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
     HeaderComponent,
-    RecipesComponent,
     BasicHighlightDirective,
     BetterHighlightDirective,
-    UnlessDirective,
-    DropDownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RecipesModule,
+    AppRoutingModule,
+    SharedDirectiveModule
   ],
   providers: [
     ShoppingService,
