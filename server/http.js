ed40705d0a83604b166009ae14ca12cb35166aaf
Spencer <swhite215@live.com>
2019-05-06 13:26:07 -0400

    example http server

--- server/http.js
+++ server/http.js
@@ -1,49 +1,7 @@
 const http = require("http");
-const fs = require("fs");
+const requestHandler = require("./httpRoutes");
 
-function requestListener(req, res) {
-    //Request Information
-    console.log(req);
-    console.log(req.method);
-    console.log(req.url);
-    console.log(req.headers);
-
-    //Variables
-    const url = req.url;
-    const method = req.method;
-
-    if (url === "/") {
-        //Response Information
-        res.setHeader("Content-Type", "text/html");
-        res.write("<h1>Hello World</h1>");
-        res.write(
-            "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
-        );
-        return res.end();
-    }
-
-    if (url === "/message" && method === "POST") {
-        let body = [];
-
-        req.on("data", chunk => {
-            body.push(chunk);
-        });
-
-        req.on("end", () => {
-            console.log(body); //Buffer Array
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody); //Parsed Buffer Array String
-            const message = parrsedBody.split("=")[1];
-
-            fs.writeFileSync("message.txt", message);
-            res.statusCode = 302;
-            res.setHeader("Location", "/");
-            return res.end();
-        });
-    }
-}
-
-const server = http.createServer(requestListener);
+const server = http.createServer(requestHandler);
 
 server.listen(3000, () => {
     console.log("Server listenig on Port 3000");
