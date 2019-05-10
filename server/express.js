4722786fb1c1a4f60f9f99b565e78e8fe29ab033
Spencer White <swhite215@live.com>
2019-05-10 14:46:01 -0400

    added example middleware function

--- server/express.js
+++ server/express.js
@@ -1,3 +1,19 @@
 const express = require("express");
 
 const app = express();
+
+//Middleware
+app.use((req, res, next) => {
+    console.log(req);
+    console.log(res);
+    console.log(next);
+
+    //Call next to allow the request to travel onto the next middleware
+    next();
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
