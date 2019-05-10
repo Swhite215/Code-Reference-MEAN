d6f3b443f58b8b708a90abbdfdd7bb625868387b
Spencer White <swhite215@live.com>
2019-05-10 15:04:03 -0400

    routing in express using external routes file

--- server/express.js
+++ server/express.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const app = express();
+const apiRoutes = require("./routes");
 
-//Middleware
+//Middleware - Applies to all requests
 app.use((req, res, next) => {
     console.log(req);
     console.log(res);
@@ -10,6 +11,19 @@ app.use((req, res, next) => {
 
     //Call next to allow the request to travel onto the next middleware
     next();
+});
+
+//Routing - Example Path
+app.use('/example', (req, res, next) => {
+    res.send(`<h1>Welcome to Example Page</h1>`);
+});
+
+//Routing - Example File
+app.use('/api', apiRoutes);
+
+//Routing - Root Path
+app.use('/', (req, res, next) => {
+    res.send(`<h1>Hello</h1>`);
 });
 
 const PORT = process.env.PORT || 3000;
