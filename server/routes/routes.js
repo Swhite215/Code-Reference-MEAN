0f953947a884551efbe39259b4ac6b7a0204fc3b
Spencer White <swhite215@live.com>
2019-05-10 15:22:18 -0400

    added example crud http methods

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-//Example Routes
+//Example Route - GET
 router.get("/sample", (req, res, next) => {
     res.send(`<h1>Hello from sample api route</h1>`);
+});
+
+//Example Route - POST
+router.post("/sample", (req, res, next) => {
+    res.send(`<h1>POST, something should be created...</h1>`);
+});
+
+//Example Route - PUT
+router.put("/sample", (req, res, next) => {
+    res.send(`<h1>PUT, somethign should be updated...</h1>`);
+});
+
+//Example Route - DELETE
+router.delete("/sample", (req, res, next) => {
+    res.send(`<h1>DELETE, something should be deleted...</h1>`);
 });
 
 router.get("/", (req,res,next) => {
