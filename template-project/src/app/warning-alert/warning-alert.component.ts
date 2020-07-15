d6e660c21cee8d9c4cef6af0a431ab35ff30ab5b
Spencer White <swhit210@ford.com>
2020-07-15 09:24:25 -0400

    added css url

--- template-project/src/app/warning-alert/warning-alert.component.ts
+++ template-project/src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core"
 
 @Component({
     selector: "app-warning-alert",
-    templateUrl: "./warning-alert.component.html"
+    templateUrl: "./warning-alert.component.html",
+    styleUrls: ["./warning-alert.component.css"]
 })
 
 export class WarningAlertComponent {
