6ff47b1bb15611f5872848952b745dada758d9ea
Spencer White <swhit210@ford.com>
2020-07-22 09:52:30 -0400

    moved comments to example file

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -25,12 +25,3 @@ export class AppComponent {
   }
 
 }
-
-// ngOnChanges - Called after a bound input property changes
-// ngOnInit - Called once the component is initialized
-// ngDoCheck - Called during every change detection run
-// ngAfterContentInit - Called after content (ng-content) has been projected into view
-// ngAfterContentChecked - Called every time the projected content has been checked
-// ngAfterViewInit - Called after the component's view (and child views) have been initialized
-// ngAfterViewChecked - Called evrey time the view (and child views) have been checked
-// ngOnDestroy - Called once the component is about to be destroyed
