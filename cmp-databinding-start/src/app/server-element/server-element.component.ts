642546397625dbf187897544f6e54f814e6114cf
Spencer White <swhit210@ford.com>
2020-07-22 09:52:37 -0400

    examples of lifecycle methods

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -9,9 +9,49 @@ export class ServerElementComponent implements OnInit {
 
   @Input() element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() {
+    console.log("Constructor called!")
+  }
 
+  // ngOnChanges - Called after a bound input property changes
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes)
+    console.log("ngOnChanges called!")
+  }
+
+  // ngOnInit - Called once the component is initialized
   ngOnInit(): void {
+    console.log("ngOnInit called!")
+  }
+
+  // ngDoCheck - Called during every change detection run
+  ngDoCheck() {
+    console.log("ngDoCheck called!")
+  }
+
+  // ngAfterContentInit - Called after content (ng-content) has been projected into view
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called!")
+  }
+
+  // ngAfterContentChecked - Called every time the projected content has been checked
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked called!")
+  }
+v
+  // ngAfterViewInit - Called after the component's view (and child views) have been initialized
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit called!")
+  }
+
+  // ngAfterViewChecked - Called evrey time the view (and child views) have been checked
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked called!")
+  }
+
+  // ngOnDestroy - Called once the component is about to be destroyed
+  ngOnDestroy() {
+    console.log("ngOnDestroy called!")
   }
 
 }
