a724b2d0570956397e49463f252413a5a6abacd7
Spencer White <swhit210@ford.com>
2020-07-23 09:48:15 -0400

    emitting recipe selected from child

--- udemy-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -8,14 +8,20 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
-    new Recipe("A Test Recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
-    new Recipe("A Test Recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+    new Recipe("Recipe One", "This is recipe number one.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("Recipe Two", "This is recipe number two.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("Recipe Three", "This is recipe number three.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
   ];
+
+  @Output() sendRecipeSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleRecipeSelected(recipe: Recipe) {
+    this.sendRecipeSelected.emit(recipe);
   }
 
 }
