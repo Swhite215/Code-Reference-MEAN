e00d380d6b559c9b4c53b1b35128fe07cbcdd707
Spencer White <swhit210@ford.com>
2020-07-23 09:17:40 -0400

    changing of nav variable

--- udemy-course-project/src/app/app.component.ts
+++ udemy-course-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-project';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
