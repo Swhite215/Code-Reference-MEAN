d71dc17d19f3ed8e3776a94f3612a602185117d1
Spencer White <swhit210@ford.com>
2020-12-16 15:14:21 -0500

    moving docker to appropriate file

--- docker/docker.sh
+++
@@ -1,8 +0,0 @@
-docker-compose -f docker-compose.yml down
-
-docker-compose -f docker-compose.yml up -d \
-container1 \
-container2 \
-containerN
-
-docker exec -e ENVIRONMENT_VARIABLE=value container command [flags]
