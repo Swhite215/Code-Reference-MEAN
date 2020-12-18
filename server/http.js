ae21722f64f9489dd959550e9c193383abf371a4
Spencer White <swhit210@ford.com>
2020-12-18 13:31:32 -0500

    consolidated in C-R-JS

--- server/http.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const requestHandler = require("./httpRoutes");
-
-const server = http.createServer(requestHandler);
-
-server.listen(3000, () => {
-    console.log("Server listenig on Port 3000");
-});
